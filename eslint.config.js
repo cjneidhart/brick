f366d339f8eba34ff8d2120bbef2fa98056f9118
Chris Neidhart <cjneidhart@gmail.com>
2024-12-06 21:52:44 -0500

    Rename eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,6 @@ import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
   ...tseslint.configs.strict,
   eslintConfigPrettier,
   {

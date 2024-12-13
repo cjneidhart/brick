bccb72483f0131106527233cee5dff4ed4155fd6
Chris Neidhart <cjneidhart@gmail.com>
2024-12-13 15:42:22 -0500

    Make it red

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -20,7 +19,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Read the Reference
           </Link>
         </div>
       </div>
@@ -32,11 +31,12 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      // title={`Hello from ${siteConfig.title}`}
+      // description="Description will go into a meta tag in <head />"
+      >
       <main>
-        <HomepageFeatures />
+      <HomepageHeader />
+        {/* <HomepageFeatures /> */}
       </main>
     </Layout>
   );
